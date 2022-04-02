b07fd54a990c3e45d02dcad4c4c44f43059f2d99
John Varney <john@jdvarney.com>
2022-04-02 13:41:37 -0400

    Initial Commit

---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by John Varney on 4/2/22.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
